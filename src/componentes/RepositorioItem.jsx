93decdddeadcebf2bbfed1a441d000afe1ca3cfc
git <kevinhu200122@gmail.com>
2023-10-08 23:51:51 -0500

    Segunda

--- src/componentes/RepositorioItem.jsx
+++ src/componentes/RepositorioItem.jsx
@@ -5,14 +5,15 @@ const RepositorioItems = (props) => (
     <View key={props.id} style={styles.containerItems}>
         <Image style={styles.imgFish} source={{uri:props.imageAnimal}}/>
         <Text style={styles.containerItemsID}>{props.name}</Text>
-        <Text>Cientify Name:({props.nameCien})</Text>
+        <Text style={styles.tituloCien}>({props.nameCien})</Text>
         <Text>Habitat: {props.habit}</Text>
         <Text>Size: {props.Size}</Text>
         <Text>Origin: {props.origin}</Text>
         <Text>High danger of extinction: {props.highDanger}</Text>
         <Text>preservation: {props.animalCare}</Text>
         <Text>characteristics: {props.characteristics}</Text>
-        <Image style={styles.imgFish} source={{uri:props.imgMap}}/>
+        <Text style={styles.tituloCenter}>Areas where they can be found</Text>
+        <Image style={styles.imgMap} source={{uri:props.imgMap}}/>
         <Text>Quantity at present: {props.quantity}</Text>
         <Text>Description: {props.description}</Text>
         <Text>Current situation: {props.state}</Text>
@@ -27,18 +28,42 @@ const styles = StyleSheet.create({
         marginTop:20,
         marginBottom:50,
         padding:10,
+        fontSize:16,
 
     },
     containerItemsID:{
         fontWeight:'bold',
         fontSize:20,
         color:'#09f',
+        marginTop:10,
         marginBottom:5,
+        textAlign:'center'
 
+    },
+    tituloCenter:{
+        textAlign:'center',
+        fontSize:20,
+        fontWeight:'bold',
+        marginBottom:5,
+        marginTop:20,
+    },
+    tituloCien:{
+        fontStyle:'italic',
+        textAlign:'center',
+        marginBottom:20,
     },
     imgFish:{     
         alignSelf:'center' ,
-        width:300,
+        width:320,
+        height:200,
+        borderRadius:5,
+        resizeMode:'cover',
+    },
+    imgMap:{
+        marginBottom:20,
+        marginTop:20,
+        alignSelf:'center' ,
+        width:320,
         height:200,
         borderRadius:5,
         resizeMode:'cover',
--- src/componentes/RepositorioLista.jsx
+++ src/componentes/RepositorioLista.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
-import { Text, View, FlatList } from 'react-native'
+import { Text, View, FlatList, StyleSheet, Image } from 'react-native'
 import DataRepositorio from './data/dataanimalnasa.json'
 import RespositorioItems from './RepositorioItem.jsx'
 
 const RepositorioLista = () => {
     return (
+        <View>
+            <Image
+        source={{ uri: 'https://media.tenor.com/URvoYDDxW0AAAAAC/zooming-out-planet-earth.gif' }} 
+        style={styles.gif}
+      />
         <FlatList
             data={DataRepositorio}
             renderItem={({ item: repo }) => (
                 <RespositorioItems {...repo} />
             )}
         />
+        </View>
     )
 }
-
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      gif: {
+        width:400,
+        height:200, 
+      }
+});
 
 export default RepositorioLista
